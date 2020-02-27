756d55d903d9156b34f24089e1a785bcd01d4fb2
WonWooS <songw@unbc.ca>
2020-02-26 23:40:44 -0800

    solved question 15

--- problems/3sum.java
+++ problems/3sum.java
@@ -1,28 +1,32 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index = nums1.length - 1;
-        --m;
-        --n;
-        while(index >= 0){
-            if(m < 0){
-                nums1[index] = nums2[n];
-                n--;
-            }
-            else if(n < 0){
-                nums1[index] = nums1[m];
-                m--;
-            }
-            else{
-                if(nums1[m] > nums2[n]){
-                    nums1[index] = nums1[m];
-                    m--;
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> output = new LinkedList();
+        
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])){
+                // with this value, problem becomes 2sum
+                int sum = 0 - nums[i];
+                int front = i+1;
+                int back = nums.length-1;
+                while(front < back){
+                    // check if 2sum is equal to sum
+                    if(nums[front] + nums[back] == sum){
+                        output.add(Arrays.asList(nums[i], nums[front], nums[back]));
+                        while(front < back && nums[front] == nums[front+1]) front++;
+                        while(front < back && nums[back] == nums[back-1]) back--;
+                        front++;
+                        back--;
+                    }
+                    else if(nums[front] + nums[back] > sum){
+                        back--;
+                    }
+                    else{
+                        front++;
+                    }
                 }
-                else{
-                    nums1[index] = nums2[n];
-                    n--;
-                }
             }
-            index--;
         }
+        return output;
     }
 }
