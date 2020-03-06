e731a5c55ac9e05c02b3c6d9d4c1152d42acb460
WonWooS <songw@unbc.ca>
2020-03-05 21:37:01 -0800

    solved question number 13

---
+++ RomantoInteger.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+        
+        for(int i = 0; i < s.length()-1; i++){
+            if(switchNumber(s.charAt(i)) < switchNumber(s.charAt(i+1))){
+                sum = sum - switchNumber(s.charAt(i));
+            }
+            else{
+                sum = sum + switchNumber(s.charAt(i));
+            }
+            
+        }
+        
+        return sum + switchNumber(s.charAt(s.length()-1));
+        
+    }
+    
+    private int switchNumber(char c){
+        switch(c){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+}
